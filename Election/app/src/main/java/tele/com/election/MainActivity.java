919b877e6048cb0428a4051a06b368eaa3d8898e
viniciusscampos <silvcamposvinicius@gmail.com>
2016-10-29 15:47:47 -0200

    Listando dispositivos móveis próximos.

--- Election/app/src/main/java/tele/com/election/MainActivity.java
+++ Election/app/src/main/java/tele/com/election/MainActivity.java
@@ -3,8 +3,10 @@ package tele.com.election;
 import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.IntentFilter;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE_BLUETOOTH = 1;
     private BluetoothAdapter bluetoothAdapter;
+    private ArrayList<String> foundDevices = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,38 +33,42 @@ public class MainActivity extends AppCompatActivity {
                 Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH);
             }
+            bluetoothAdapter.startDiscovery();
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(bdReceiver,filter);
         }
     }
 
+    @Override
+    protected void onDestroy(){
+        unregisterReceiver(bdReceiver);
+        super.onDestroy();
+    }
 
     public void listDevices(View view) {
-        final ArrayList<String> foundDevices = new ArrayList<String>();
-        StringBuilder sb = new StringBuilder();
-        this.bluetoothAdapter.startDiscovery();
-        final BroadcastReceiver bdReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                String action = intent.getAction();
-                if(BluetoothDevice.ACTION_FOUND.equals(action)){
-                    BluetoothDevice btdevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                    foundDevices.add(btdevice.getName() + "\n" + btdevice.getAddress());
-                }
-            }
-        };
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        Intent teste = registerReceiver(bdReceiver,filter,null,null);
-        this.bluetoothAdapter.cancelDiscovery();
-
-        for(String st: foundDevices){
-            sb.append(st);
-        }
         Context context = getApplicationContext();
         int length = Toast.LENGTH_LONG;
-        Toast toast = Toast.makeText(context,sb.toString(),length);
-        toast.show();
-
-        unregisterReceiver(bdReceiver);
+        try{
+            synchronized (this){
+                wait(10000);
+            }
+            Toast toast = Toast.makeText(context,foundDevices.toString(),length);
+            toast.show();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+
+    final BroadcastReceiver bdReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                BluetoothDevice btdevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                foundDevices.add(btdevice.getName() + "\n" + btdevice.getAddress());
+            }
+        }
+    };
 
     public void makeVisible(View view) {
         Intent isDiscoverable = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
