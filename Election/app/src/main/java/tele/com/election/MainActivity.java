32d5634deb82676d4e5c106e3774a813acd3169e
viniciusscampos <silvcamposvinicius@gmail.com>
2016-10-29 19:06:19 -0200

    Pareando com dispositivo.

--- Election/app/src/main/java/tele/com/election/MainActivity.java
+++ Election/app/src/main/java/tele/com/election/MainActivity.java
@@ -1,12 +1,11 @@
 package tele.com.election;
 
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.IntentFilter;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
@@ -16,15 +15,27 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.UUID;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE_BLUETOOTH = 1;
     private BluetoothAdapter bluetoothAdapter;
-    private ArrayList<String> foundDevices = new ArrayList<String>();
+
+    private ArrayList<BluetoothDevice> foundDevices = new ArrayList<BluetoothDevice>();
+
+    private BluetoothSocket btSocket = null;
+    private BluetoothDevice btDevice = null;
+
+    private InputStream btInStream = null;
+    private OutputStream btOutStream = null;
+
+    private final UUID myuuid = UUID.randomUUID();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
             IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
             registerReceiver(bdReceiver,filter);
         }
+
     }
 
     @Override
@@ -62,12 +74,26 @@ public class MainActivity extends AppCompatActivity {
             //////////////////////
 
             ListView lv = (ListView)findViewById(R.id.listview1);
-            lv.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, foundDevices));
+            lv.setAdapter(new ArrayAdapter<BluetoothDevice>(this, android.R.layout.simple_list_item_1,foundDevices));
 
             lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-                    Toast toast = Toast.makeText(context,"clique",length);
+                    BluetoothDevice device = (BluetoothDevice) parent.getAdapter().getItem(position);
+
+                    // Cria o socket utilizando o device escolhido e o mac address do device que realizou a busca
+                    // Conecta o socket
+                    try {
+                        btSocket = device.createRfcommSocketToServiceRecord(myuuid);
+                        btSocket.connect();
+                        btInStream = btSocket.getInputStream();
+                        btOutStream = btSocket.getOutputStream();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    //Toast toast = Toast.makeText(context,device,length);
+                    Toast toast = Toast.makeText(context,bluetoothAdapter.getBondedDevices().toString(),length);
+
                     toast.show();
                 }
             });
@@ -85,7 +111,8 @@ public class MainActivity extends AppCompatActivity {
             String action = intent.getAction();
             if(BluetoothDevice.ACTION_FOUND.equals(action)){
                 BluetoothDevice btdevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                foundDevices.add(btdevice.getName() + "\n" + btdevice.getAddress());
+                //foundDevices.add(btdevice.getName() + "\n" + btdevice.getAddress());
+                foundDevices.add(btdevice);
             }
         }
     };
